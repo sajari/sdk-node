e1360c39b217674fd95b31eb548f2e7aab158fbf
Benjamin Hinchley <benjaminhinchley@gmail.com>
2019-01-18 14:43:38 +1100

    schema: move schema.field def to field.ts

--- src/schema/index.ts
+++ src/schema/index.ts
@@ -1,4 +1,4 @@
-import { field, Field, FieldOptions, Type } from "./field";
+import { fields } from "./field";
 import { mutation } from "./mutation";
 
 export {
@@ -12,25 +12,6 @@ export { FieldMode } from "./field";
 export { Schema } from "./client";
 
 export default {
-  field: {
-    string(name: string, options: FieldOptions): Field {
-      return field(Type.String, name, options);
-    },
-    integer(name: string, options: FieldOptions): Field {
-      return field(Type.Integer, name, options);
-    },
-    float(name: string, options: FieldOptions): Field {
-      return field(Type.Float, name, options);
-    },
-    double(name: string, options: FieldOptions): Field {
-      return field(Type.Double, name, options);
-    },
-    boolean(name: string, options: FieldOptions): Field {
-      return field(Type.Boolean, name, options);
-    },
-    timestamp(name: string, options: FieldOptions): Field {
-      return field(Type.Timestamp, name, options);
-    }
-  },
+  field: fields,
   mutation
 };
