99bf88a6b5b463d21c98c7a1740c8db6c11b82ff
Benjamin Hinchley <benjaminhinchley@gmail.com>
2019-01-18 14:43:38 +1100

    schema: move schema.field def to field.ts

--- src/schema/field.ts
+++ src/schema/field.ts
@@ -198,3 +198,24 @@ export function field(type: Type, name: string, options?: FieldOptions): Field {
     mode: options.mode as FieldMode
   };
 }
+
+export const fields = {
+  string(name: string, options: FieldOptions): Field {
+    return field(Type.String, name, options);
+  },
+  integer(name: string, options: FieldOptions): Field {
+    return field(Type.Integer, name, options);
+  },
+  float(name: string, options: FieldOptions): Field {
+    return field(Type.Float, name, options);
+  },
+  double(name: string, options: FieldOptions): Field {
+    return field(Type.Double, name, options);
+  },
+  boolean(name: string, options: FieldOptions): Field {
+    return field(Type.Boolean, name, options);
+  },
+  timestamp(name: string, options: FieldOptions): Field {
+    return field(Type.Timestamp, name, options);
+  }
+}
